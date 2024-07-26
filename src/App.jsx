01de79d8fcc5ccc5335acef78c075d1c945d62a0
krishn404 <maharshikrishnakant@gmail.com>
2024-07-26 07:51:52 +0530

    Integrate Google Analytics into React Vite project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './css/Global.css'
+import './css/Global.css';
 import "@fontsource/inter";
-import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import LoginButton from './components/LoginToSpotify.jsx';
 import Callback from './components/Callback.jsx';
 import { GeneratePage } from './pages/Generate.jsx';
 
-function App() {
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("Tracking page view for:", location.pathname + location.search);  // Add this line
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
+
   return (
     <Routes>
-        <Route path="/" element={<GeneratePage></GeneratePage>}></Route>
-        <Route path="/callback" element={<Callback></Callback>}></Route>
+      <Route path="/" element={<GeneratePage />}></Route>
+      <Route path="/callback" element={<Callback />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/analytics.js
@@ -0,0 +1,7 @@
+import ReactGA from 'react-ga4';
+
+const initializeAnalytics = () => {
+  ReactGA.initialize('G-FS096WNPD0'); // Replace with your Tracking ID
+};
+
+export default initializeAnalytics;
